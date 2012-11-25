2fe811d6a4cd043260c692340363714fda2d5967
Richard Harrah <topplethenun@hawksharbor.info>
2012-11-25 15:56:06 -0500

    a - repair subcommand

--- src/com/modcrafting/diablodrops/commands/DiabloDropCommand.java
+++ src/com/modcrafting/diablodrops/commands/DiabloDropCommand.java
@@ -24,9 +24,10 @@ import com.modcrafting.toolapi.lib.Tool;
 public class DiabloDropCommand implements CommandExecutor
 {
 	private DiabloDrops plugin;
+
 	public DiabloDropCommand(DiabloDrops plugin)
 	{
-		this.plugin=plugin;
+		this.plugin = plugin;
 	}
 
 	public boolean onCommand(CommandSender sender, Command command,
@@ -77,66 +78,101 @@ public class DiabloDropCommand implements CommandExecutor
 				}
 				if (args[0].equalsIgnoreCase("modify"))
 				{
-					if(args.length<2) return true;
+					if (args.length < 2)
+						return true;
 					if (args[1].equalsIgnoreCase("lore"))
 					{
-						String lore = combineSplit(2,args," ");
-						lore = ChatColor.translateAlternateColorCodes("&".toCharArray()[0], lore);
-						new Tool(player.getItemInHand()).setLore(Arrays.asList(lore.split(",")));
-						player.sendMessage(ChatColor.GREEN+"Set the lore for the item!");
+						String lore = combineSplit(2, args, " ");
+						lore = ChatColor.translateAlternateColorCodes(
+								"&".toCharArray()[0], lore);
+						new Tool(player.getItemInHand()).setLore(Arrays
+								.asList(lore.split(",")));
+						player.sendMessage(ChatColor.GREEN
+								+ "Set the lore for the item!");
 						return true;
 					}
 					if (args[1].equalsIgnoreCase("name"))
 					{
-						String name = combineSplit(2,args," ");
-						name = ChatColor.translateAlternateColorCodes("&".toCharArray()[0], name);
+						String name = combineSplit(2, args, " ");
+						name = ChatColor.translateAlternateColorCodes(
+								"&".toCharArray()[0], name);
 						new Tool(player.getItemInHand()).setName(name);
-						player.sendMessage(ChatColor.GREEN+"Set the name for the item!");
+						player.sendMessage(ChatColor.GREEN
+								+ "Set the name for the item!");
 						return true;
 					}
 					if (args[1].equalsIgnoreCase("enchant"))
 					{
-						if(args.length<4) return true;
-						if(args[2].equalsIgnoreCase("add"))
+						if (args.length < 4)
+							return true;
+						if (args[2].equalsIgnoreCase("add"))
 						{
-							if(args.length<5) return true;
-							int i=1;
-							try{
+							if (args.length < 5)
+								return true;
+							int i = 1;
+							try
+							{
 								i = Integer.parseInt(args[4]);
-							}catch (NumberFormatException nfe){
-								i=1;
 							}
-							Enchantment ech = Enchantment.getByName(args[3].toUpperCase());
-							if(ech!=null){
-								player.getItemInHand().addUnsafeEnchantment(ech, i);
-								player.sendMessage(ChatColor.GREEN+"Added enchantment.");
-							}else{
-								player.sendMessage(ChatColor.RED+args[3]+" :enchantment does not exist!");
+							catch (NumberFormatException nfe)
+							{
+								i = 1;
+							}
+							Enchantment ech = Enchantment.getByName(args[3]
+									.toUpperCase());
+							if (ech != null)
+							{
+								player.getItemInHand().addUnsafeEnchantment(
+										ech, i);
+								player.sendMessage(ChatColor.GREEN
+										+ "Added enchantment.");
+							}
+							else
+							{
+								player.sendMessage(ChatColor.RED + args[3]
+										+ " :enchantment does not exist!");
 							}
 							return true;
 						}
-						if(args[2].equalsIgnoreCase("remove"))
-						{		
+						if (args[2].equalsIgnoreCase("remove"))
+						{
 							ItemStack is = player.getItemInHand();
-							Map<Enchantment,Integer> hm = new HashMap<Enchantment,Integer>();
-							for(Enchantment e1:is.getEnchantments().keySet()){
-								if(!e1.getName().equalsIgnoreCase(args[3])){
+							Map<Enchantment, Integer> hm = new HashMap<Enchantment, Integer>();
+							for (Enchantment e1 : is.getEnchantments().keySet())
+							{
+								if (!e1.getName().equalsIgnoreCase(args[3]))
+								{
 									hm.put(e1, is.getEnchantmentLevel(e1));
 								}
 							}
 							is.addUnsafeEnchantments(hm);
-							player.sendMessage(ChatColor.GREEN+"Removed enchantment.");
+							player.sendMessage(ChatColor.GREEN
+									+ "Removed enchantment.");
 							return true;
-							
+
 						}
 					}
 				}
-				if (args[0].equalsIgnoreCase("reload")&&sender.hasPermission("diablodrops.reload")){
+				if (args[0].equalsIgnoreCase("reload")
+						&& sender.hasPermission("diablodrops.reload"))
+				{
 					plugin.getServer().getPluginManager().disablePlugin(plugin);
 					plugin.getServer().getPluginManager().enablePlugin(plugin);
 					plugin.reloadConfig();
 					plugin.getLogger().info("Reloaded");
-					sender.sendMessage(ChatColor.GREEN+"DiabloDrops Reloaded");
+					sender.sendMessage(ChatColor.GREEN + "DiabloDrops Reloaded");
+					return true;
+				}
+				if (args[0].equalsIgnoreCase("repair"))
+				{
+					if (plugin.dropsAPI.canBeItem(player.getItemInHand()
+							.getType()))
+					{
+						player.getItemInHand().setDurability((short) 0);
+						player.sendMessage(ChatColor.GREEN + "Item repaired.");
+						return true;
+					}
+					player.sendMessage("Unable to repair item.");
 					return true;
 				}
 				if (plugin.dropsAPI.matchesTier(args[0]))
@@ -160,14 +196,19 @@ public class DiabloDropCommand implements CommandExecutor
 				return true;
 		}
 	}
-	public String combineSplit(int startIndex, String[] string, String seperator) {
+
+	public String combineSplit(int startIndex, String[] string, String seperator)
+	{
 		StringBuilder builder = new StringBuilder();
-		if(string.length >= 1){
-			for (int i = startIndex; i < string.length; i++) {
+		if (string.length >= 1)
+		{
+			for (int i = startIndex; i < string.length; i++)
+			{
 				builder.append(string[i]);
 				builder.append(seperator);
 			}
-			if(builder.length() > seperator.length()){
+			if (builder.length() > seperator.length())
+			{
 				builder.deleteCharAt(builder.length() - seperator.length()); // remove
 				return builder.toString();
 			}

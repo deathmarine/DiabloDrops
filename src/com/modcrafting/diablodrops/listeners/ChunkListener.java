4593fa961c1b9f82538f6d7caa7beeacecc6c03b
Richard Harrah <topplethenun@hawksharbor.info>
2012-12-01 00:23:33 -0500

    adding obsidian under lava

--- src/com/modcrafting/diablodrops/listeners/ChunkListener.java
+++ src/com/modcrafting/diablodrops/listeners/ChunkListener.java
@@ -231,6 +231,7 @@ public class ChunkListener implements Listener
         for (Block b2 : blockSurround)
         {
             b2.getRelative(0, -1, 0).setType(Material.LAVA);
+            b2.getRelative(0, -2, 0).setType(Material.OBSIDIAN);
             b2.setType(Material.GLASS);
             switch (plugin.gen.nextInt(7))
             {
@@ -293,10 +294,13 @@ public class ChunkListener implements Listener
             return;
         }
         block.setType(Material.CHEST);
-        if(plugin.gen.nextBoolean()){
-        	addRuin1Pattern(block);
-        }else{
-        	deathRuin(block,allowedTypes);
+        if (plugin.gen.nextBoolean())
+        {
+            addRuin1Pattern(block);
+        }
+        else
+        {
+            deathRuin(block, allowedTypes);
         }
         if ((block.getState() instanceof Chest))
             return;
@@ -412,27 +416,38 @@ public class ChunkListener implements Listener
         startWWW.setTypeIdAndData(blockType, (byte) plugin.gen.nextInt(4),
                 false);
     }
-    private void deathRuin(Block block,List<Material> mats){
-    	Block under = block.getRelative(BlockFace.DOWN);
-    	int square = plugin.gen.nextInt(9);
-    	Location loc = under.getLocation();
-		double maxX = Math.max(loc.getX()-square, loc.getX()+square);
-		double maxZ = Math.max(loc.getZ()-square, loc.getZ()+square);
-		double minX = Math.min(loc.getX()-square, loc.getX()+square);
-		double minZ = Math.min(loc.getZ()-square, loc.getZ()+square);
-		
-		for(double i=0;i<=Math.abs(maxX-minX);i++){
-			for(double ii=0;ii<=Math.abs(maxZ-minZ);ii++){
-					Location nt = new Location(loc.getWorld(), minX+i, loc.getY(), minZ+ii);
-					if(i==0||ii==0){
-						for(int iii=plugin.gen.nextInt(6);iii>0;iii--){
-							Location t = new Location(loc.getWorld(), minX+i, loc.getY()+iii, minZ+ii);
-							t.getBlock().setTypeId(mats.get(plugin.gen.nextInt(mats.size())).getId());
-						}
-					}
-					nt.getBlock().setTypeId(mats.get(plugin.gen.nextInt(mats.size())).getId());
-				
-			}
-		}	
+
+    private void deathRuin(Block block, List<Material> mats)
+    {
+        Block under = block.getRelative(BlockFace.DOWN);
+        int square = plugin.gen.nextInt(9);
+        Location loc = under.getLocation();
+        double maxX = Math.max(loc.getX() - square, loc.getX() + square);
+        double maxZ = Math.max(loc.getZ() - square, loc.getZ() + square);
+        double minX = Math.min(loc.getX() - square, loc.getX() + square);
+        double minZ = Math.min(loc.getZ() - square, loc.getZ() + square);
+
+        for (double i = 0; i <= Math.abs(maxX - minX); i++)
+        {
+            for (double ii = 0; ii <= Math.abs(maxZ - minZ); ii++)
+            {
+                Location nt = new Location(loc.getWorld(), minX + i,
+                        loc.getY(), minZ + ii);
+                if (i == 0 || ii == 0)
+                {
+                    for (int iii = plugin.gen.nextInt(6); iii > 0; iii--)
+                    {
+                        Location t = new Location(loc.getWorld(), minX + i,
+                                loc.getY() + iii, minZ + ii);
+                        t.getBlock().setTypeId(
+                                mats.get(plugin.gen.nextInt(mats.size()))
+                                        .getId());
+                    }
+                }
+                nt.getBlock().setTypeId(
+                        mats.get(plugin.gen.nextInt(mats.size())).getId());
+
+            }
+        }
     }
 }

6349e061765b8888e06cb2c3081b5e8f0fc1770f
deathmarine <josh@mcjoshmod.com>
2012-12-07 13:54:32 -0500

    Socket Merge

--- src/com/modcrafting/diablodrops/listeners/MobListener.java
+++ src/com/modcrafting/diablodrops/listeners/MobListener.java
@@ -112,7 +112,6 @@ public class MobListener implements Listener
             }
         }
     }
-
     @EventHandler(priority = EventPriority.HIGH)
     public void onSpawn(CreatureSpawnEvent event)
     {

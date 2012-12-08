7601ef21b8093f21015482c814b95373087a8619
Richard Harrah <topplethenun@hawksharbor.info>
2012-12-08 16:20:32 -0500

    f - tag NPE hopefully

--- src/com/modcrafting/toolapi/lib/Tool.java
+++ src/com/modcrafting/toolapi/lib/Tool.java
@@ -13,34 +13,38 @@ import org.bukkit.craftbukkit.inventory.CraftItemStack;
 
 public class Tool extends CraftItemStack implements ToolInterface
 {
-    public Tool(ItemStack item)
+    public Tool(final ItemStack item)
     {
         super(item);
         tag();
     }
 
-    public Tool(Material mat)
+    public Tool(final Material mat)
     {
         super(mat, 1);
         tag();
     }
 
-    public Tool(org.bukkit.inventory.ItemStack source)
+    public Tool(final org.bukkit.inventory.ItemStack source)
     {
         super(((CraftItemStack) source).getHandle());
         tag();
     }
 
     @Override
-    public void addLore(String string)
+    public void addLore(final String string)
     {
         NBTTagCompound tag = getTag();
         NBTTagCompound ntag = tag.getCompound("display");
         if (ntag == null)
+        {
             ntag = new NBTTagCompound("display");
+        }
         NBTTagList p = ntag.getList("Lore");
         if (p == null)
+        {
             p = new NBTTagList("Lore");
+        }
         p.add(new NBTTagString("", string));
         ntag.set("Lore", p);
         tag.setCompound("display", ntag);
@@ -53,12 +57,14 @@ public class Tool extends CraftItemStack implements ToolInterface
         NBTTagCompound tag = getTag();
         ArrayList<String> strings = new ArrayList<String>();
         String[] lores = new String[] {};
-        if (tag == null || tag.getCompound("display") == null
-                || tag.getCompound("display").getList("Lore") == null)
+        if ((tag == null) || (tag.getCompound("display") == null)
+                || (tag.getCompound("display").getList("Lore") == null))
             return lores;
         NBTTagList list = tag.getCompound("display").getList("Lore");
         for (int i = 0; i < list.size(); i++)
+        {
             strings.add(((NBTTagString) list.get(i)).data);
+        }
         strings.toArray(lores);
         return lores;
     }
@@ -68,8 +74,8 @@ public class Tool extends CraftItemStack implements ToolInterface
     {
         NBTTagCompound tag = getTag();
         ArrayList<String> strings = new ArrayList<String>();
-        if (tag == null || tag.getCompound("display") == null
-                || tag.getCompound("display").getList("Lore") == null)
+        if ((tag == null) || (tag.getCompound("display") == null)
+                || (tag.getCompound("display").getList("Lore") == null))
             return strings;
         NBTTagList list = tag.getCompound("display").getList("Lore");
         for (int i = 0; i < list.size(); i++)
@@ -77,8 +83,10 @@ public class Tool extends CraftItemStack implements ToolInterface
             NBTTagString n = (NBTTagString) list.get(i);
             if (n != null)
             {
-                if (n.data != null || !(n.data.length() < 1))
+                if ((n.data != null) || !(n.data.length() < 1))
+                {
                     strings.add(n.data);
+                }
             }
         }
         return strings;
@@ -87,28 +95,30 @@ public class Tool extends CraftItemStack implements ToolInterface
     @Override
     public String getName()
     {
-        return this.getHandle().r();
+        return getHandle().r();
     }
 
     @Override
     public Integer getRepairCost()
     {
-        return this.getHandle().getRepairCost();
+        return getHandle().getRepairCost();
     }
 
     @Override
     public NBTTagCompound getTag()
     {
-        return this.getHandle().getTag();
+        return getHandle().getTag();
     }
 
     @Override
-    public void setLore(List<String> lore)
+    public void setLore(final List<String> lore)
     {
         NBTTagCompound tag = getTag();
         NBTTagCompound ntag = tag.getCompound("display");
         if (ntag == null)
+        {
             ntag = new NBTTagCompound();
+        }
         NBTTagList p = new NBTTagList();
         for (String s : lore)
         {
@@ -120,27 +130,27 @@ public class Tool extends CraftItemStack implements ToolInterface
     }
 
     @Override
-    public void setName(String name)
+    public void setName(final String name)
     {
-        this.getHandle().c(name);
+        getHandle().c(name);
     }
 
     @Override
-    public void setRepairCost(Integer i)
+    public void setRepairCost(final Integer i)
     {
-        this.getHandle().setRepairCost(i);
+        getHandle().setRepairCost(i);
     }
 
     @Override
-    public void setTag(NBTTagCompound tag)
+    public void setTag(final NBTTagCompound tag)
     {
-        this.getHandle().setTag(tag);
+        getHandle().setTag(tag);
     }
 
     private void tag()
     {
-        ItemStack oItem = this.getHandle();
-        if (!oItem.hasTag())
+        ItemStack oItem = getHandle();
+        if ((getTag() == null) || !oItem.hasTag())
         {
             setTag(new NBTTagCompound());
         }
